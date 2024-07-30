4993ab3f0e0bd210b0f200e9e686cb00ccb181d1
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-07-31 01:57:02 +0800

    所有產品圖、產品詳細頁面都用objectFit cover,新增圖片

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -5,6 +5,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import ProductImage from '/public/images/朋朋衛生紙商品圖.jpg'
+import ProductImage2 from '/public/images/coat1.jpg'
+import ProductImage3 from '/public/images/coat2.jpg'
+import ProductImage4 from '/public/images/coat3.jpg'
+import ProductImage5 from '/public/images/coat4.jpg'
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import Image from "next/image";
@@ -17,7 +21,13 @@ export default function ProductsPage() {
         router.push("/products/7aa1aas61cx1vs6d54fa96")
     }
 
-    const products = [1, 2, 3, 4, 5]
+    const products = [
+        {id:1,pic:ProductImage},
+        {id:2,pic:ProductImage2},
+        {id:3,pic:ProductImage3},
+        {id:4,pic:ProductImage4},
+        {id:5,pic:ProductImage5}
+        ]
 
     return (
         <Box sx={{ p: 2 }}>
@@ -28,7 +38,7 @@ export default function ProductsPage() {
 
 
                 {products.map((product) => (
-                    <Grid item lg={2} md={2} sm={4} xs={4} key={product}>
+                    <Grid item lg={2} md={2} sm={4} xs={4} key={product.id}>
                         <Card sx={{ boxShadow: "none" }}>
                             <CardMedia onClick={goToProductDetail} sx={{'&:hover': { cursor: "pointer" }}}>
 
@@ -37,12 +47,12 @@ export default function ProductsPage() {
                                         position: 'relative',
                                         width: '100%',
                                         height: 0,
-                                        paddingBottom: '100%', // 这是根据宽高比计算的
+                                        paddingBottom: '120%', // 这是根据宽高比计算的
                                         overflow: 'hidden',
                                     }}
                                 >
                                     <Image
-                                        src={ProductImage}
+                                        src={product.pic}
                                         alt="product information"
                                         fill
                                         style={{objectFit:"cover"}}
