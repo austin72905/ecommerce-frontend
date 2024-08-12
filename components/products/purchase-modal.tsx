88198aefd4972d70dd9ec4f93b0132f9ae5a7d8b
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-08-12 20:41:43 +0800

    獨立gridcontent 組件出來

--- components/products/purchase-modal.tsx
+++ components/products/purchase-modal.tsx
@@ -6,6 +6,7 @@ import { ProductInfomation } from "@/interfaces";
 import Image from "next/image";
 import { useState } from "react";
 import { useCartStore } from "@/store/store";
+import GridContent from "../ui/gridContent";
 
 
 export default function PurchaseModal({ product, modalOpen, handleModalOpen, handleModalClose }: PurchaseModalProps) {
@@ -81,10 +82,10 @@ export default function PurchaseModal({ product, modalOpen, handleModalOpen, han
                             />
 
 
-                            <Grid container columns={8} rowSpacing={3} alignItems={"center"} >
-                                <Grid item xs={8} md={3} sx={{border:"0px solid black" }}>
+                            <Grid sx={{my:1}} container columns={8} rowSpacing={3} alignItems={"center"} >
+                                <Grid item xs={8} md={3} sx={{ border: "0px solid black" }}>
 
-                                    <Box sx={{ maxHeight: "300px", maxWidth: "200px",border:"0px solid black",position:"relative",left:"50%",transform: 'translateX(-50%)', }}>
+                                    <Box sx={{ maxHeight: "300px", maxWidth: "200px", border: "0px solid black", position: "relative", left: "50%", transform: 'translateX(-50%)', }}>
                                         {product.coverImg &&
                                             <Box
                                                 sx={{
@@ -112,109 +113,113 @@ export default function PurchaseModal({ product, modalOpen, handleModalOpen, han
                                 </Grid>
                                 <Grid item xs={8} md={5}>
                                     <CardContent sx={{ p: 0 }}>
-                                        <Grid container alignItems={"center"}  columns={columns} rowSpacing={2} sx={{ px: 1, width: "100%"}} >
-                                            <Grid item xs={8} sx={{ px: 0 }}>
-                                                
-                                                    <Typography variant='h5' sx={{ fontWeight: "bold", mx: 0}}>{product.title}</Typography>
-                                                
-
+                                        <Grid container alignItems={"center"} columns={columns} rowSpacing={2} sx={{ px: 1, width: "100%" }} >
+                                            <Grid item xs={columns} sx={{ px: 0 }}>
+                                                <Typography variant='h5' sx={{ fontWeight: "bold", mx: 0 }}>{product.title}</Typography>
                                             </Grid>
-
-                                            <Grid item xs={xs} sm={sm} md={md} lg={lg}>
-                                                <Typography variant='body2'>售價</Typography>
-                                            </Grid>
-
-                                            <Grid item xs={contentxs} sm={contentsm} md={contentmd} lg={contentlg}>
-                                                <Stack direction={"row"} sx={{ alignItems: "center" }} spacing={1}>
-                                                    <Typography variant='subtitle1' sx={{ textDecoration: "line-through", color: "red" }}>${product.price}</Typography>
-                                                    <Typography sx={{ fontWeight: "bold", fontSize: "24px" }}>${product.price}</Typography>
-                                                </Stack>
-                                            </Grid>
-
-                                            <Grid item xs={xs} sm={sm} md={md} lg={lg}>
-                                                <Typography variant='body2'>顏色</Typography>
-                                            </Grid>
-
-                                            <Grid item xs={contentxs} sm={contentsm} md={contentmd} lg={contentlg}>
-
-                                                {/*顏色 */}
-                                                <Grid container columns={8} spacing={1}>
-                                                    {
-                                                        product.color
-                                                            ?
-                                                            product.color.map((s, index) => (
-
-                                                                <Grid key={s} item xs={2} sm={2.5} md={2} lg={1}>
-                                                                    <Stack onClick={() => { setSelectColor(s) }} alignItems={"center"}
-                                                                        sx={{ border: s === selectColor ? "1px solid #61D1BD" : "1px solid #d9d9d9", width: "30px", p: 0.5, borderRadius: "4px", cursor: "pointer" }}>
-                                                                        <Box sx={{ background: s, minWidth: "30px", minHeight: "30px" }}></Box>
-
-                                                                    </Stack>
-                                                                </Grid>
-
-
-                                                            ))
-                                                            :
-                                                            <Grid item xs={2} sm={2.5} md={2} lg={1}>
-                                                                <Stack alignItems={"center"} sx={{ border: "1px solid #d9d9d9", p: 0.5, borderRadius: "4px" }}>
-                                                                    <Typography sx={{ color: "#AFAFAF" }} variant='caption'>標準</Typography>
-                                                                </Stack>
-                                                            </Grid>
+                                            {/*售價 */}
+                                            <Grid item xs={columns}>
+                                                <GridContent
+                                                    xs={xs} sm={sm} md={md} lg={lg} columns={columns}
+                                                    title={<Typography variant='body2'>售價</Typography>}
+                                                    content={
+                                                        <Stack direction={"row"} sx={{ alignItems: "center" }} spacing={1}>
+                                                            <Typography variant='subtitle1' sx={{ textDecoration: "line-through", color: "red" }}>${product.price}</Typography>
+                                                            <Typography sx={{ fontWeight: "bold", fontSize: "24px" }}>${product.price}</Typography>
+                                                        </Stack>
                                                     }
-                                                </Grid>
-
-
+                                                />
                                             </Grid>
 
-                                            <Grid item xs={xs} sm={sm} md={md} lg={lg}>
-                                                <Typography variant='body2'>規格</Typography>
-                                            </Grid>
+                                            {/*顏色 */}
+                                            <Grid item xs={columns}>
+                                                <GridContent
+                                                    title={<Typography variant='body2'>顏色</Typography>}
+                                                    content={
+                                                        <Grid container columns={8} spacing={1}>
+                                                            {
+                                                                product.color
+                                                                    ?
+                                                                    product.color.map((s, index) => (
 
-                                            <Grid item xs={contentxs} sm={contentsm} md={contentmd} lg={contentlg}>
-                                                {/*規格 */}
-                                                <Grid container columns={8} spacing={1}>
-                                                    {
-                                                        product.size
-                                                            ?
-                                                            product.size.map((s, index) => (
+                                                                        <Grid key={s} item xs={2} sm={2.5} md={2} lg={1}>
+                                                                            <Stack onClick={() => { setSelectColor(s) }} alignItems={"center"}
+                                                                                sx={{ border: s === selectColor ? "1px solid #61D1BD" : "1px solid #d9d9d9", width: "30px", p: 0.5, borderRadius: "4px", cursor: "pointer" }}>
+                                                                                <Box sx={{ background: s, minWidth: "30px", minHeight: "30px" }}></Box>
 
-                                                                <Grid key={s} item xs={2} sm={2.5} md={2} lg={1}>
-                                                                    <Stack onClick={() => { setSelectSize(s) }} alignItems={"center"} sx={{ border: s === selectSize ? "1px solid #61D1BD" : "1px solid #d9d9d9", minWidth: "25px", minHeight: "25px", p: 0.5, borderRadius: "4px", cursor: "pointer" }}>
-                                                                        <Typography >{s}</Typography>
-                                                                    </Stack>
-                                                                </Grid>
+                                                                            </Stack>
+                                                                        </Grid>
 
 
-                                                            ))
-                                                            :
+                                                                    ))
+                                                                    :
+                                                                    <Grid item xs={2} sm={2.5} md={2} lg={1}>
+                                                                        <Stack alignItems={"center"} sx={{ border: "1px solid #d9d9d9", p: 0.5, borderRadius: "4px" }}>
+                                                                            <Typography sx={{ color: "#AFAFAF" }} variant='caption'>標準</Typography>
+                                                                        </Stack>
+                                                                    </Grid>
 
-                                                            <Grid item xs={2} sm={2.5} md={2} lg={1}>
-                                                                <Stack alignItems={"center"} sx={{ border: "1px solid #d9d9d9", p: 0.5, borderRadius: "4px" }}>
-                                                                    <Typography sx={{ color: "#AFAFAF" }} variant='caption'>標準</Typography>
-                                                                </Stack>
-                                                            </Grid>
+                                                            }
+
+                                                        </Grid>
 
                                                     }
-                                                </Grid>
+                                                    columns={columns}
+                                                    xs={xs} sm={sm} md={md} lg={lg}
+                                                />
 
                                             </Grid>
+                                            {/*規格 */}
+                                            <Grid item xs={columns}>
+
+                                                <GridContent
+                                                    title={<Typography variant='body2'>規格</Typography>}
+                                                    content={
+                                                        <Grid container columns={8} spacing={1}>
+                                                            {
+                                                                product.size
+                                                                    ?
+                                                                    product.size.map((s, index) => (
+
+                                                                        <Grid key={s} item xs={2} sm={2.5} md={2} lg={1}>
+                                                                            <Stack onClick={() => { setSelectSize(s) }} alignItems={"center"} sx={{ border: s === selectSize ? "1px solid #61D1BD" : "1px solid #d9d9d9", minWidth: "25px", minHeight: "25px", p: 0.5, borderRadius: "4px", cursor: "pointer" }}>
+                                                                                <Typography >{s}</Typography>
+                                                                            </Stack>
+                                                                        </Grid>
+
+
+                                                                    ))
+                                                                    :
+
+                                                                    <Grid item xs={2} sm={2.5} md={2} lg={1}>
+                                                                        <Stack alignItems={"center"} sx={{ border: "1px solid #d9d9d9", p: 0.5, borderRadius: "4px" }}>
+                                                                            <Typography sx={{ color: "#AFAFAF" }} variant='caption'>標準</Typography>
+                                                                        </Stack>
+                                                                    </Grid>
+
+                                                            }
+                                                        </Grid>
+                                                    }
+                                                    columns={columns}
+                                                    xs={xs} sm={sm} md={md} lg={lg}
+                                                />
+
+
+                                            </Grid>
+
 
                                         </Grid>
                                     </CardContent>
                                 </Grid>
-                                <Grid item md={8}>
-
-                                </Grid>
+                                
                             </Grid>
-                            <CardActions sx={{ px: 0 }}>
-
+                            <CardActions sx={{ px: 1,my:1 }}>
                                 <Box sx={{ display: "flex", alignItems: "center", width: "50%" }}>
                                     <RemoveIcon onClick={handleCountMinus} sx={{ "&:hover": { cursor: "pointer" }, color: "#AFAFAF", border: "solid 1px", height: "30px", width: "30px", borderRadius: "4px" }} />
                                     <TextFieldWrapper value={itemCount} size='small' inputProps={{ style: { textAlign: "center" } }} ></TextFieldWrapper>
                                     <AddIcon onClick={handleCountPlus} sx={{ "&:hover": { cursor: "pointer" }, color: "#AFAFAF", border: "solid 1px", height: "30px", width: "30px", borderRadius: "4px" }} />
                                 </Box>
                                 <Button variant="outlined" disableRipple sx={{ flexGrow: 1 }} onClick={addProductToCart}>加入購物車</Button>
-
                             </CardActions>
 
                         </Card>
---
+++ components/ui/gridContent.tsx
@@ -0,0 +1,34 @@
+import { Grid } from "@mui/material";
+import { ReactNode } from "react";
+
+export default function GridContent({ xs, sm, md, lg, xl, columns,title,content }: GridContentProps) {
+
+    const cxs= columns && xs? columns - xs:undefined;
+    const csm =  columns && sm? columns - sm:undefined;
+    const cmd= columns && md? columns - md:undefined;
+    const clg= columns && lg? columns - lg:undefined;
+    const cxl = columns && xl? columns - xl:undefined;
+
+    return (
+        <Grid container columns={columns} alignItems={"center"}>
+            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                {title}
+            </Grid>
+            <Grid item xs={cxs} sm={csm} md={cmd} lg={clg} xl={cxl}>
+                {content}
+            </Grid>
+        </Grid>
+    )
+}
+
+
+interface GridContentProps {
+    xs?: number;
+    sm?: number;
+    md?: number;
+    lg?: number;
+    xl?: number;
+    columns?: number;
+    title:ReactNode;
+    content:ReactNode;
+}
