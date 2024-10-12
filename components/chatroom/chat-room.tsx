1218628bf871c3ab469aa3bfa8efa0ad8c581161
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-10-12 15:55:52 +0800

    使用socket.io 實作聊天室 初版

--- components/chatroom/chat-room.tsx
+++ components/chatroom/chat-room.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Badge, Box, InputBase, List, ListItem, ListItemAvatar, ListItemText, Paper, Stack, Typography, useMediaQuery, useTheme } from "@mui/material"
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import LogoImage from '@/public/images/logo.jpg'
 
 
@@ -7,6 +7,13 @@ import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import SendIcon from '@mui/icons-material/Send';
+import { io, Socket } from "socket.io-client";
+
+
+import 'dayjs/locale/zh-tw'; 
+import dayjs from 'dayjs';
+
+dayjs.locale('zh-tw');
 
 interface ChatRoomProps {
     chatOpen: boolean;
@@ -15,6 +22,71 @@ interface ChatRoomProps {
 
 const ChatRoom = ({ chatOpen, handleToggleChatRoom }: ChatRoomProps) => {
 
+    const [msg, setMsg] = useState<string>("")
+    const [recieveMsges, setRecieveMsges] = useState<Chat[]>([]);
+
+
+    useEffect(() => {
+        socket = io('http://localhost:3333',{
+            withCredentials:true
+        }) // 連接socket.io 服務器
+
+        // 監聽服務器傳來的訊息
+        socket.on('send-message', (data: Chat) => {
+            console.log("get a message from server:",data)
+            setRecieveMsges((prevMsgs) => [...prevMsgs, data])
+        })
+
+        socket.on('join-room-request',({ roomId })=>{
+            socket.emit('join-room', { roomId });
+        })
+
+        // 斷開 連結
+        return () => {
+            if (socket) {
+                socket.disconnect();
+            }
+        }
+
+    }, [])
+
+    // 傳送訊息
+    const sendMsg = () => {
+        if(msg===""){
+            return
+        }
+        const formattedDate = dayjs().format('YYYY/M/D A hh:mm');
+        const data: Chat = {
+            isAdmin: false,
+            msg: msg,
+            date: formattedDate,
+            isTodayFirstMsg: true,
+            reciever_id:"admin",
+            sender_id:"test"
+        }
+        // 因為socketio 只送給對方，不送給自己
+        setRecieveMsges((prevMsgs) => [...prevMsgs, data])
+        socket.emit('message-from-client', data);
+        setMsg('')
+    }
+
+    // 處理按下鍵盤事件
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            sendMsg(); // 按下 Enter 觸發送訊息
+        }
+    };
+
+    const messagesEndRef = useRef<HTMLDivElement | null>(null); // 用於滾動到最下方
+
+    // 當新訊息添加時，自動滾動到最下方
+    useEffect(() => {
+        //只有自己輸入的訊息要滑到最下方
+        if(recieveMsges.length>0 &&!recieveMsges[recieveMsges.length-1].isAdmin){
+            messagesEndRef.current?.scrollIntoView({ behavior: 'auto' });
+        }
+        
+    }, [recieveMsges]); // 監聽 messages 的變化
 
     const theme = useTheme()
     const isXSScreen: boolean = useMediaQuery(('(max-width:480px)'))
@@ -26,7 +98,7 @@ const ChatRoom = ({ chatOpen, handleToggleChatRoom }: ChatRoomProps) => {
      * 
      */
     return (
-        <Box sx={{ bottom: "0px", right: isXSScreen?"0px":"30px", width: isXSScreen?"100%":"280px", height: isXSScreen?"100%":"435px", display: chatOpen ? "block" : "none", border: "0px solid #9c9c9c", position: "fixed", zIndex: 9999 }} >
+        <Box sx={{ bottom: "0px", right: isXSScreen ? "0px" : "30px", width: isXSScreen ? "100%" : "280px", height: isXSScreen ? "100%" : "435px", display: chatOpen ? "block" : "none", border: "0px solid #9c9c9c", position: "fixed", zIndex: 9999 }} >
             <Paper sx={{ width: "100%", height: "100%", border: "0px solid #9c9c9c" }}>
 
                 <Stack direction={"column"} sx={{ height: "100%" }}>
@@ -53,10 +125,10 @@ const ChatRoom = ({ chatOpen, handleToggleChatRoom }: ChatRoomProps) => {
 
                     </Stack>
                     {/*聊天紀錄 */}
-                    <Stack flexGrow={1} sx={{overflow: 'auto',overflowX:"hidden"}}>
-                        <List sx={{ width: "100%" ,border: "0px solid red" }}>
+                    <Stack flexGrow={1} sx={{ overflow: 'auto', overflowX: "hidden" }}>
+                        <List sx={{ width: "100%", border: "0px solid red" }}>
                             {
-                                chatRecord.map((chat, index) =>
+                                recieveMsges.map((chat, index) =>
                                 (
 
                                     <React.Fragment key={index}>
@@ -94,10 +166,13 @@ const ChatRoom = ({ chatOpen, handleToggleChatRoom }: ChatRoomProps) => {
                                     </React.Fragment>
 
                                 ))
+                                
+                                
                             }
 
                         </List>
-
+                         {/* 自動滾動到這個 div */}
+                        <div ref={messagesEndRef} />
                     </Stack>
 
 
@@ -111,9 +186,12 @@ const ChatRoom = ({ chatOpen, handleToggleChatRoom }: ChatRoomProps) => {
                                 sx={{ ml: 1, flex: 1 }}
                                 placeholder="輸入訊息"
                                 inputProps={{ 'aria-label': '輸入訊息' }}
+                                onChange={(e) => setMsg(e.target.value)}
+                                value={msg}
+                                onKeyDown={handleKeyPress}
                             />
 
-                            <SendIcon sx={{ p: '10px', width: "20px", height: "20px", borderRadius: "0px", '&:hover': { cursor: "pointer" } }} aria-label="send" />
+                            <SendIcon sx={{ p: '10px', width: "20px", height: "20px", borderRadius: "0px", '&:hover': { cursor: "pointer" } }} aria-label="send" onClick={sendMsg} />
 
                         </Paper>
                     </Stack>
@@ -133,6 +211,10 @@ interface Chat {
     msg: string;
     date: string;
     isTodayFirstMsg: boolean;
+    sender_id:string;
+    reciever_id:string;
+    chatroom_id?:string;
+    timeStamp?:string;
 }
 
 
@@ -232,4 +314,19 @@ const chatRecord: Chat[] = [
 
 ]
 
+
+
+//socket.io
+
+let socket: Socket
+
+interface ServerToClientEvents {
+    message: (data: Chat) => void;
+}
+
+interface ClientToServerEvents {
+    message: (data: Chat) => void;
+}
+
 export default ChatRoom;
+
