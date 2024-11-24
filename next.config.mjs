613d9a95accb1ad53f7cc2b41713b6232e9022dd
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-11-24 10:24:58 +0800

    修改images配置 為新版配置方式

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com','localhost','ponggoodbf.com'], // 允許的圖像域名
+    remotePatterns: [
+      {
+        protocol: 'https', // 協議
+        hostname: 'lh3.googleusercontent.com', // 允許的域名
+      },
+      {
+        protocol: 'http', // 協議，根據你的使用情況選擇
+        hostname: 'localhost', // 允許本地測試圖片
+      },
+      {
+        protocol: 'https', // 協議
+        hostname: 'ponggoodbf.com', // 你的圖片服務器域名
+      },
+    ],
   },
 };
 
