c1cc2f4f70662e84bc09d7404f52fb329873c6c6
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-11-27 16:35:06 +0800

    登出時，同步前端購物車到後端

--- components/layout/user-menu.tsx
+++ components/layout/user-menu.tsx
@@ -1,3 +1,6 @@
+import { ProductInfomationCount } from "@/interfaces";
+import { ApiResponse } from "@/interfaces/api/response";
+import { Cart, CartItem, mergeCartContent } from "@/pages/cart";
 import { useAlertMsgStore, useCartStore, userUserInfoStore, useSubscribeListStore } from "@/store/store";
 import { Menu, MenuItem, Typography } from "@mui/material";
 import Link from "next/link";
@@ -13,11 +16,15 @@ export default function UserMenu({ anchorElement, handleCancelAnchor }: UserMenu
     const setUserInfo = userUserInfoStore((state) => state.setUserInfo)
     const setAlertMsg = useAlertMsgStore((state) => state.setAlertMsg)
     const initializeCart = useCartStore((state) => state.initializeCart)
+    const cartContent = useCartStore((state) => state.cartContent)
 
 
     const clearSubscribeIdList = useSubscribeListStore((state) => state.clearSubscribeIdList)
     const handleLogout = async () => {
         console.log("logout")
+
+        // 清空購物車前應該要將原本購物車內容同步到後端
+        await syncCartcontent(cartContent)
 
         const apiUrl= process.env.NEXT_PUBLIC_BACKEND_URL
         await fetch(`${apiUrl}/User/UserLogout`, {
@@ -27,11 +34,40 @@ export default function UserMenu({ anchorElement, handleCancelAnchor }: UserMenu
         setAlertMsg("已成功登出")
         setUserInfo(null)
         clearSubscribeIdList()
+        // 已經使用zustand persist 所以註解此行
         // 登出時將 購物車清掉，因為登入時，已經加入的購物車
-        localStorage.removeItem('cart');
+        //localStorage.removeItem('cart');
+
+        
+        
         initializeCart([])
         goToLogin()
 
+    }
+
+    const syncCartcontent =async (content: ProductInfomationCount[])=>{
+        try {
+            console.log(" syncCartcontent 合併購物車")
+            const cartItems = content.map(item => {
+                const cartItem: CartItem = {
+                    productVariantId: item.selectedVariant?.variantID,
+                    quantity: item.count
+                }
+
+                return cartItem;
+            })
+            const cart: Cart = {
+                items: cartItems,
+                isCover: true
+            }
+            const result = await mergeCartContent(cart) as ApiResponse;
+
+            //console.log("mergeCartContent result=", result)
+
+
+        } catch (error) {
+            console.error('Error syncCartcontent data:', error)
+        }
     }
 
     const goToLogin=()=>{
